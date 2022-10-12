451abbfad7a2bf8e4c4f511120a0705fd35017ea
AKey001 <anton.kiessling@web.de>
2022-10-12 20:09:41 +0200

    theme colors updated

--- studium/lib/main.dart
+++ studium/lib/main.dart
@@ -1,20 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:studium/theme/color_schemes.dart';
 import 'package:studium/theme/theme.dart';
 
 import 'main_layout.dart';
 
 void main() {
-  runApp(const Main());
+  runApp(Main());
 }
 
 class Main extends StatelessWidget {
-  const Main({Key? key}) : super(key: key);
+  Main({Key? key}) : super(key: key);
+
+  ThemeData lightTheme = ThemeData(useMaterial3: true, colorScheme: lightColorScheme);
+  ThemeData darkTheme = ThemeData(useMaterial3: true, colorScheme: darkColorScheme);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       restorationScopeId: 'root',
-      theme: appTheme(),
+      // theme: appTheme(),
+      theme: ThemeData(useMaterial3: true, colorScheme: lightColorScheme).copyWith(
+        textTheme: buildTextTheme(lightTheme.textTheme),
+      ),
+      darkTheme: darkTheme.copyWith(
+        scaffoldBackgroundColor: const Color(0xFF1A1B1E),
+        textTheme: buildTextTheme(darkTheme.textTheme),
+      ),
       home: const HomeLayout(),
     );
   }
--- studium/lib/theme/theme.dart
+++ studium/lib/theme/theme.dart
@@ -1,30 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 
-ThemeData appTheme() {
-  ThemeData base = ThemeData.dark();
-  return base.copyWith(
-    useMaterial3: true,
-    scaffoldBackgroundColor: const Color.fromRGBO(50, 50, 50, 1.0),
-    colorScheme: _buildColorScheme(base),
-    textTheme: _buildTextTheme(base.textTheme),
-  );
-}
-
-ColorScheme _buildColorScheme(ThemeData base) {
-  return base.colorScheme.copyWith(
-    primary: const Color(0xff1565c0),
-    secondary: const Color(0xff90caf9),
-    surface: const Color.fromRGBO(50, 50, 50, 1),
-    surfaceTint: const Color.fromRGBO(21, 101, 192, 1.0),
-    onSurface: Colors.white,
-    onSurfaceVariant: Colors.grey.shade300,
-    secondaryContainer: const Color(0xff1565c0),
-    onSecondaryContainer: Colors.white,
-  );
-}
-
-TextTheme _buildTextTheme(TextTheme base) {
+TextTheme buildTextTheme(TextTheme base) {
   return base.copyWith(
     headline5: base.headline5!.copyWith(
       fontWeight: FontWeight.w500,
@@ -42,7 +18,5 @@ TextTheme _buildTextTheme(TextTheme base) {
     ),
   ).apply(
     fontFamily: 'Rubik',
-    displayColor: Colors.grey.shade400,
-    bodyColor: Colors.grey.shade300,
   );
 }
